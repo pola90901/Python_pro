0d1ab21f24593b9fb1ee7981f139ee8d6c927705
pola helmy <117487838+pola90901@users.noreply.github.com>
2023-01-17 01:22:41 +0200

    Add files via upload

--- linux/mouse.py
+++ linux/mouse.py
@@ -3,4 +3,4 @@ f = open( "/dev/input/event3", "rb" );
 # Open the file in the read-binary mode
 while 1:
   data = f.read(3)  # Reads the 3 bytes 
-  print struct.unpack('3b',data)  
+  print struct.unpack('3b',data)  #Unpacks the bytes to integers
